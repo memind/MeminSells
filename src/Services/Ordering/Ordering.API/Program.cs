10662547293d7356c6b3b211524e2e1e41eb7cda
Mehmet Emin Dağ <mehmetemin.dag@bilgeadamboost.com>
2023-08-04 23:25:45 +0300

    - Ordering Service, CQRS, MediatR, FluentValidations, AutoMapper

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.Configuration;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistance;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
