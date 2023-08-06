f55d6b29506ee7fb95aba6c1a1a8651fc361faf2
Mehmet Emin Dağ <mehmetemin.dag@bilgeadamboost.com>
2023-08-07 01:20:51 +0300

    - Ocelot integrations

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.Hosting.Internal;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration)
+                .AddCacheManager(settings => settings.WithDictionaryHandle());
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.UseOcelot();
+app.Run();
